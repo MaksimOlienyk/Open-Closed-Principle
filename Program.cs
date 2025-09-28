f8d636a05613105b63ba20847208a84a7b118f75
maljaethereal <121117752+MaksimOlienyk@users.noreply.github.com>
2025-09-28 18:16:43 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Program
+{
+    abstract class Weapon
+    {
+        public abstract void Invoke();
+    }
+
+    class Gun : Weapon
+    {
+        public override void Invoke()
+        {
+            Console.WriteLine("Piv");
+        }
+    }
+
+    class Knife : Weapon
+    {
+        public override void Invoke()
+        {
+            Console.WriteLine("Vjuh");
+        }
+    }
+    class Rifle : Weapon
+    {
+        public override void Invoke()
+        {
+            // Логіка стрільби
+        }
+    }
+}
